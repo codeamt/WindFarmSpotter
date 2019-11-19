389dc953c852f7eadbf9d97c46c263aba6535409
AnnMargaret Tutu <amt@blaqbox.us>
2019-11-19 02:28:31 -0500

    Added a simple frontend

--- web_engines/windfarm_spotter_py/engine/routes.py
+++ web_engines/windfarm_spotter_py/engine/routes.py
@@ -4,6 +4,7 @@ import time
 import requests
 import os
 import base64
+import glob
 from engine import app
 from engine.windfarmspotter_model import *
 from werkzeug.utils import secure_filename
@@ -11,13 +12,14 @@ from werkzeug.utils import secure_filename
 
 @app.route("/", methods=["GET"])
 def index():
+  samples = glob.glob("%s/*" % app.config['SAMPLE_FOLDER'])
   instructions = "Welcome to WindSpotter!\n\nThis API exposes a model for inferring wind farm capacity of \
-  satellite imagery input.\n\nSample test images are located in the root > data > test.\n\nThe API is comprised of the following endpoints:"
-  return jsonify(instructions)
-  #if request.args['type'] == 'json':
-  #  return jsonify(instructions)
-  #else:
-  #return render_template('index.html', summary=instructions)
+  satellite imagery input.\n\nSample test images are located in the root > static > test_imgs.\n\nThe API is comprised of the following endpoints:\n\n/predict\n\n\n\nTo run inference on a sample image, make a POST request to the prediction endpoint.\n\n\n\nExample request:\n\n\n\ncurl -X POST http://127.0.0.1:5000/predict -H 'Content-Type: application/json'  -d '{'file':'~/wfs_inference_py/engine/static/test_imgs/test_low_capacity_7.jpg'}'"
+  return jsonify({"instructions": instructions, "sample_file_names": samples})
+  if request.args['type'] == 'json':
+    return jsonify({"instructions": instructions, "sample_file_names": samples})
+  else:
+  return render_template('index.html', samples=samples)
 
 
 @app.route("/predict", methods=["POST"])
@@ -25,25 +27,39 @@ def predict():
   windfarmspotter_model = WindFarmSpotter(app.config["PATH"])
   label_map = {
      "turbines_low_capacity": "Low Capacity Wind Farm",
-     "turbines_med_capacity": "Medium Capacity Wind Farm",
+     "turbines_medium_capacity": "Medium Capacity Wind Farm",
      "turbines_high_capacity": "High Capacity Wind Farm",
      "no_turbines_no_potential": "Not a Turbine Farm",
      "no_turbines_low_potential": "Potential Low Capacity Wind Farm",
      "no_turbines_med_potential": "Potential Medium Capacity Wind Farm",
      "no_turbines_high_potential": "Potential High Capacity Wind Farm"
   }
-  user_input = request.get_json()["file"]
-  print(user_input)
-  file_data = Path(user_input).read_bytes()
-  #bytes_array = data.read_bytes()
-  app.logger.info("Classifying satellite image %s" % (Path(user_input).name),)
-  img = open_image(BytesIO(file_data))
-  t = time.time()
-  pred_class, pred_idx, outputs = windfarmspotter_model.model.predict(img)
-  dt = time.time() - t
-  app.logger.info("Execution time: %0.02f seconds" % (dt))
-  app.logger.info("Image %s classified as %s" % (Path(user_input).name, pred_class))
+
+
   if request.is_json:
+    user_input = request.get_json()["file"]
+    print(user_input)
+    file_data = Path(user_input).read_bytes()
+    #bytes_array = data.read_bytes()
+    app.logger.info("Classifying satellite image %s" % (Path(user_input).name),)
+    img = open_image(BytesIO(file_data))
+    t = time.time()
+    pred_class, pred_idx, outputs = windfarmspotter_model.model.predict(img)
+    dt = time.time() - t
+    app.logger.info("Execution time: %0.02f seconds" % (dt))
+    app.logger.info("Image %s classified as %s" % (Path(user_input).name, pred_class))
     return jsonify({"prediction": label_map[str(pred_class)]})
   else:
-    return render_template('prediction.html', prediction=label_map[pred_class])
+    user_input = request.form["file"]#request.files.get('file')
+    #basename = Path(user_input).name
+    print(user_input)
+    full_url = app.config["PATH"] + "/engine" + user_input
+    file_data = Path(full_url).read_bytes()
+    app.logger.info("Classifying satellite image %s" % (Path(user_input).name),)
+    img = open_image(BytesIO(file_data))
+    t = time.time()
+    pred_class, pred_idx, outputs = windfarmspotter_model.model.predict(img)
+    dt = time.time() - t
+    app.logger.info("Execution time: %0.02f seconds" % (dt))
+    app.logger.info("Image %s classified as %s" % (Path(user_input).name, pred_class))
+    return jsonify({"prediction": label_map[str(pred_class)]})
