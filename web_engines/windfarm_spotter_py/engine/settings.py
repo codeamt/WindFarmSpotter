c334c686b2abc0eae135a3af6e620d9906bc46ed
AnnMargaret Tutu <amt@blaqbox.us>
2019-11-21 15:37:30 -0500

    clean master

--- web_engines/windfarm_spotter_py/engine/settings.py
+++
@@ -1,5 +0,0 @@
-DEBUG = True
-PORT=5000
-PATH = "."
-SAMPLE_FOLDER = 'engine/static/test_imgs'
-PIP_PACKAGES = "/pip-packages/site-packages/"
--- web_engines/windfarm_spotter_py/run.py
+++
@@ -1,5 +0,0 @@
-from engine import app
-from engine.windfarmspotter_model import *
-
-if __name__ == "__main__":
-  app.run(host="127.0.0.1", debug=app.config['DEBUG'], port=app.config["PORT"], threaded=True)
